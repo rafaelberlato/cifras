a1b41d1e62a60e055e5e83814b7fefb559010a56
rafaelberlato <rcberlato@gmail.com>
2023-09-27 23:50:54 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "127.0.0.1:3306";
-$username = "u695430671_berlatoadmin";
-$password = "R4F43L@#B34L4T0.@37#a@";
-$dbname = "u695430671_sistemas";
+$servername = ${{ secrets.servername }};
+$username = ${{ secrets.username }};
+$password = ${{ secrets.password }};
+$dbname = {{ secrets.dbname }};
 
 // Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
